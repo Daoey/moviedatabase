1ff8f245f3ee382104a3f609bbba30d8d087420a
Johan Åsling <johan.asling@hotmail.com>
2016-09-25 10:40:48 +0200

    Renamed project

---
+++ moviedatabase/src/main/java/se/joas/angular2mavenblueprint/App.java
@@ -0,0 +1,13 @@
+package se.joas.angular2mavenblueprint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
